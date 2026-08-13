8d6859d454545143df2a2b68fee5b54329f47e30
Antonio Fernández Flambó <antoniofdezflambo@gmail.com>
2026-08-13 18:43:11 +0200

    Exercise 8.4 Completed: Author's Books

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -124,7 +124,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (args.author) {
+        return books.filter(book => book.author === args.author)
+      }
+      return books
+    },
     allAuthors: () => authors.map(author => {
       const bookCount = books.filter(book => book.author === author.name).length
       return {
