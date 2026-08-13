f697199a8dfe0b8c5d0a437e769d215f299465ac
Antonio Fernández Flambó <antoniofdezflambo@gmail.com>
2026-08-13 18:45:58 +0200

    Exercise 8.5 Completed: Genre's Books

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -127,6 +127,9 @@ const resolvers = {
     allBooks: (root, args) => {
       if (args.author) {
         return books.filter(book => book.author === args.author)
+      }
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre))
       }
       return books
     },
